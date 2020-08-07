fcce54c4f52925e4ac731c76cbec1b5217487fba
LesediMaleka <lesedi@adadev.org.za>
2020-08-07 17:08:57 +0200

    added the task 5,6,11 and 10

--- js/sandbox.js
+++ js/sandbox.js
@@ -26,9 +26,9 @@ const MainNumber = 65;
 let numberOne = 16;
 let numberTwo = 49;
 
-let sum = numberOne + numberTwo ;
+let Totalsum = numberOne + numberTwo ;
 
-if (sum === MainNumber || numberOne === MainNumber || numberTwo === MainNumber){
+if (Totalsum === MainNumber || numberOne === MainNumber || numberTwo === MainNumber){
     console.log("true");
 }else{
     console.log("false");
@@ -49,7 +49,23 @@ if (NumberThree === FinalInput && Mainsum === FinalInput ){
     console.log("false");
 };
 
+//Task 5
 
+let base = 4;
+let height = 4;
+
+let area =1/2 * (base*height);
+ console.log(area)
+
+// Task 6
+const NumberArray = [18,20,23,46,48,15]
+
+NumberArray.sort()
+
+NumberArray.sort((a,b) => a-b)
+
+const max = NumberArray[NumberArray.length-1]
+console.log(max);
 // Task 7
 
 // A
@@ -68,11 +84,11 @@ function fahrenheitToCelsius(fahrenheit)
 {
   let FahrenheitTemp = fahrenheit;
   let FahrenheitToCelsius = (FahrenheitTemp - 32) * 5 / 9;
-  let message = FahrenheitTemp+'°F is equal to' + FahrenheitToCelsius + '°C.';
+  let message = FahrenheitTemp+'°F is equal to ' + FahrenheitToCelsius + ' °C.';
     console.log(message);
 } 
 
-fahrenheitToCelsius(15);
+fahrenheitToCelsius(23);
 
 
 // Task 8
@@ -84,35 +100,75 @@ function timeConvert(n) {
     var rhours = Math.floor(hours);
     var minutes = (hours - rhours) * 60;
     var rminutes = Math.round(minutes);
-    return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
+    return num + " minutes = " + rhours + " hour(s) and " + rminutes + "  minute(s).";
     }
     
-    console.log(timeConvert(200));
+    console.log(timeConvert());
 
     // eg.2
-function timeConverter(n) {
-    var number = 133;
-    var hours = (number / 60);
-    var remainhours = Math.floor(hours);
-    var minutes = (hours - remainhours) * 60;
-    var remainingminutes = Math.round(minutes);
-    return number + " minutes = " + remainhours + " hour(s) and " + remainingminutes + " minute(s).";
-    }
+// function timeConverter(n) {
+//     var number = 133;
+//     var hours = (number / 60);
+//     var remainhours = Math.floor(hours);
+//     var minutes = (hours - remainhours) * 60;
+//     var remainingminutes = Math.round(minutes);
+//     return number + " minutes = " + remainhours + " hour(s) and " + remainingminutes + " minute(s).";
+//     }
     
-    console.log(timeConverter(200));
+//     console.log(timeConverter(200));
 
-    // Task 10
-let multiples = function (n) {
-    let sum = 0;
-    for (let i = 1; i < n; i++) {
-        if ((i % 3 == 0) || (i % 5 == 0)) {
-            sum += i;
+    // Task 9
+    // function multiplesOfThousand(number) {
+    //     let sum = 0;
+    //     for (let i = 0; i <= number; i++) {
+      
+    //       if (i % 3 === 0 || i % 5 === 0) {
+    //         sum += i;
+    //       }
+    //     }
+    //     return sum;
+    //   }
+      
+    //   console.log( multiplesOfThousand(10) );
+
+    let multiples = function (n) {
+        let Totalsum = 0;
+        for (let i = 1; i < n; i++) {
+            if ((i % 3 == 0) || (i % 5 == 0)) {
+                Totalsum += i;
+            }
         }
-    }
-    document.write(sum);
-};
+        document.write(Totalsum);
+    };
+    
+    
+    multiples(1000);
+    
+    // console.log(Totalsum);
+      
 
 
-multiples(1000);
+// Task 10
+let vowels = "aeiouAEIOU";
+let string = "HELLO challenge.Pre BOOTCAMP challenges";
 
-console.log(sum);
+let array = "";
+for (i = 0; i < string.length; i++){
+    if(vowels.indexOf(string[i]) >= 0) array += string[i];
+
+}
+console.log(array);
+
+// Task 11
+function CharactersCommon(string1, string2) {
+    let string1ToArray = Array.from(string1);
+    let string2ToArray = Array.from(string2);
+
+let commonCharacters = string2ToArray.filter(value => {
+    return string1ToArray.indexOf(value) != -1;
+});
+
+console.log(`Common characters are: ${commonCharacters}`);
+}
+
+CharactersCommon('house', 'computers');
